c2348328ffc6e42413f0a1d73ff95b0df093a050
bracco23 <bracco23592@hotmail.it>
2017-08-01 16:17:28 +0200

    Update lib.php
    Modifica struttura, passaggio da metodo privato a parametro per il recupero del body

--- Server/backend/include/lib.php
+++ Server/backend/include/lib.php
@@ -1,33 +1,56 @@
-<?php
-	abstract class RESTItem{
-		
-		public static $ERROR_NODATA = array('errno' => 1, 'errdesc' => 'Dati insufficienti');
-		
-		function __construct($db){
-			$this->db = $db;
-		}
-		
-		public function dispatch(){
-			switch($_SERVER['REQUEST_METHOD']){
-			case 'GET': 
-				$this->do_get();
-				break;
-			case 'POST':
-				$this->do_post();
-				break;
-			case 'DELETE':
-				$this->do_del();
-				break;
-			}
-		}
-		
-		protected function get_body(){
-			return json_decode(file_get_contents('php://input'), true);
-		}
-		
-		abstract protected function do_get();
-		abstract protected function do_post();
-		abstract protected function do_del();
-		
-	};
+<?php
+
+	abstract class RESTItem{
+
+		
+
+		public static $ERROR_NODATA = array('errno' => 1, 'errdesc' => 'Dati insufficienti');
+
+		
+
+		function __construct($db){
+
+			$this->db = $db;
+
+		}
+
+		
+
+		public function dispatch(){
+			$body = json_decode(file_get_contents('php://input'), true);
+
+			switch($_SERVER['REQUEST_METHOD']){
+
+			case 'GET': 
+
+				$this->do_get($body);
+
+				break;
+
+			case 'POST':
+
+				$this->do_post($body);
+
+				break;
+
+			case 'DELETE':
+
+				$this->do_del($body);
+
+				break;
+
+			}
+
+		}		
+
+		abstract protected function do_get($body);
+
+		abstract protected function do_post($body);
+
+		abstract protected function do_del($body);
+
+		
+
+	};
+
 ?>
