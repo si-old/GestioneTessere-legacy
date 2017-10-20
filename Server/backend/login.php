dd5fa2ba3ccf9ac796fd26181cf193a251b3a4a8
emiddio esposito <emiddio@20160424-AIT.prod.altengroup.dir>
2017-10-20 21:33:13 +0200

    modificato

--- Server/backend/login.php
+++ Server/backend/login.php
@@ -1,66 +1,78 @@
 <?php
-	require_once('include/config.php');
-	
-	//contiene RESTItem, base per le richieste
-	require_once('include/lib.php');
-	
-	class Login extends RESTItem{
+    require_once('include/config.php');
+    
+    //contiene RESTItem, base per le richieste
+    require_once('include/lib.php');
+    
+class Login extends RESTItem
+{
 
-		function __construct($db, $admin_user, $admin_password){
-			parent::__construct($db);
-			$this->admin_user = $admin_user;
-			$this->admin_password = $admin_password;
-		}
-		
-		private function check_login($user, $password){
-			if(strcasecmp($user, $this->admin_user)==0 && strcmp($password, $this->admin_user)==0){
-				return true;
-			}
-			$stmt = $this->db->prepare('SELECT * FROM Direttivo WHERE User=? and Password=? ');
-			$stmt->bind_param('ss', $user, $password);
-			if( ! $stmt->execute() ){
-				throw new RESTException(HttpStatusCode::$INTERNAL_SERVER_ERROR, $this->db->error);
-			}
-			$stmt->bind_result($idr, $user, $pass, $socio);
-			if($stmt->fetch()){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		
-		protected function do_get(){
-			throw new RESTException(HttpStatusCode::$METHOD_NOT_ALLOWED);
-		}
-		
-		protected function do_post($data){
-			if(isset($data['user']) && isset($data['password'])){
-				$user = $data['user'];
-				$password = $data['password'];
-				$is_admin = strcasecmp($user, $this->admin_user)==0;
-				$successful = $this->check_login($user, $password);
-				if($successful){
-					$this->session->create($is_admin);
-				}
-				return array('login' => $successful, 'admin' => $is_admin);
-			}else{
-				throw new RESTException(HttpStatusCode::$BAD_REQUEST, "Request JSON object is missing or has a wrong format");	
-			}
-		}
-		
-		protected function do_del(){
-			$res = $this->session->destroy();
-			return array('login' => !$res, 'admin' => false);
-		}
+    function __construct($db, $admin_user, $admin_password)
+    {
+        parent::__construct($db);
+        $this->admin_user = $admin_user;
+        $this->admin_password = $admin_password;
+    }
+        
+    private function check_login($user, $password)
+    {
+        if (strcasecmp($user, $this->admin_user)==0 && strcmp($password, $this->admin_user)==0) {
+            return true;
+        }
+        $stmt = $this->db->prepare('SELECT * FROM Direttivo WHERE User=? and Password=? ');
+        $stmt->bind_param('ss', $user, $password);
+        if (! $stmt->execute()) {
+            throw new RESTException(HttpStatusCode::$INTERNAL_SERVER_ERROR, $this->db->error);
+        }
+        $stmt->bind_result($idr, $user, $pass, $socio);
+        if ($stmt->fetch()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+        
+    protected function do_get()
+    {
+        throw new RESTException(HttpStatusCode::$METHOD_NOT_ALLOWED);
+    }
+        
+    /*
+    {
+        user: username dell'utente che vuole loggarsi
+        password: password dell'utente che vuole loggarsi
+    }
+    */
+    protected function do_post($data)
+    {
+        if (isset($data['user']) && isset($data['password'])) {
+            $user = $data['user'];
+            $password = $data['password'];
+            $is_admin = strcasecmp($user, $this->admin_user)==0;
+            $successful = $this->check_login($user, $password);
+            if ($successful) {
+                $this->session->create($is_admin);
+            }
+            return array('login' => $successful, 'admin' => $is_admin);
+        } else {
+            throw new RESTException(HttpStatusCode::$BAD_REQUEST, "Request JSON object is missing or has a wrong format");
+        }
+    }
+        
+    protected function do_del()
+    {
+        $res = $this->session->destroy();
+        return array('login' => !$res, 'admin' => false);
+    }
 
-		protected function is_session_authorized(){
-			return true;
-		}
-	}
-	
-	// $db ho dovuto portarla dentro col costruttore di RESTItem
-	// user e password per l'admin sono iniettate col costruttore locale.
-	// tutte e tre sono definite in config.php
-	$temp = new Login($db, $ADMIN_USER, $ADMIN_PSW);
-	$temp->dispatch();
-?>
+    protected function is_session_authorized()
+    {
+        return true;
+    }
+}
+    
+    // $db ho dovuto portarla dentro col costruttore di RESTItem
+    // user e password per l'admin sono iniettate col costruttore locale.
+    // tutte e tre sono definite in config.php
+    $temp = new Login($db, $ADMIN_USER, $ADMIN_PSW);
+    $temp->dispatch();
