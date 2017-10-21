5bd2daba5c372809c220a9dcb82376be7380e12a
bracco23 <emiddio.esposito@hotmail.it>
2017-10-22 00:45:26 +0200

    aggiunta funzionalita al form, aggiunto pulsante di invio

--- Server/frontend/src/app/mail/mail-form.component.ts
+++ Server/frontend/src/app/mail/mail-form.component.ts
@@ -13,12 +13,42 @@ export class MailFormComponent implements OnInit {
 
     corsi: Corso[];
 
+    oggetto: string = '';
+    corpo: string = '';
+    email_feedback: string = '';
+
+    blacklist: boolean = false;
+    tutti: boolean = false;
+
+    lavoratori: boolean = false;
+    lavoratori_disabled: boolean = false;
+
+    corsi_checked: boolean[] = [];
+    corsi_disabled: boolean[] = [];
+
     constructor(private corsisrv: CorsiService) {
     }
 
     ngOnInit() {
         this.corsisrv.getCorsi().subscribe(
-            (in_corsi: Corso[]) => { this.corsi = in_corsi }
+            (in_corsi: Corso[]) => { 
+                in_corsi.forEach(
+                    (corso: Corso) => {
+                        this.corsi_checked[corso.id] = false;
+                        this.corsi_disabled[corso.id] = false;
+                    }
+                );
+                this.corsi = in_corsi
+            }
+        )
+    }
+
+    tuttiCallback(){
+        this.lavoratori_disabled = this.tutti;
+        this.corsi_disabled.forEach(
+            (val: boolean, i: number) => {
+                this.corsi_disabled[i] = this.tutti;
+            }
         )
     }
 }
