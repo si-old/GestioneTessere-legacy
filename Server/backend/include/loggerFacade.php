bdf6e363e0e89122f8c512e954a52e54722daf06
bracco23 <emiddio.esposito@hotmail.it>
2017-10-23 15:41:55 +0200

    modifica al facade, aggiunta funzioni di comodo a lib

--- Server/backend/include/loggerFacade.php
+++ Server/backend/include/loggerFacade.php
@@ -4,17 +4,44 @@ require_once('config.php');
 
 class LoggerFacade {
 
-    function __construct($class) {
+    function __construct($class, $db) {
         $this->logger = Logger::getLogger($class);
+        $this->db = $db;
     }
 
-    public function log($message, $level) {
-        $this->logger->log($level, $message);
-    }
-
-    public static function prepare_message($user, $message) {
+    private function prepare_message($user, $message) {
         $array = array("Utente" => $user, "Messaggio" => $message);
         return json_encode($array, JSON_UNESCAPED_UNICODE);
+    }
+
+    public function info($user, $message) {
+        $log_message = $this->prepare_message($user, $message);
+        $this->logger->info($log_message);
+    }
+
+    public function error($user, $message) {
+        $log_message = $this->prepare_message($user, $message);
+        $this->logger->error($log_message);
+    }
+
+    public function get_log_messages(){
+        $results = $this->db->query('SELECT timestamp, logger, level, message FROM Log WHERE 1');
+        $to_return = [];
+        while($row = $results->fetch_assoc()){
+            $decoded = json_decode($row['message'], true);
+            $to_return[] = [
+                            'orario' => $row['timestamp'],
+                            'origine' => $row['logger'],
+                            'livello' => $row['level'],
+                            'utente' => $decoded['Utente'],
+                            'messaggio' => $decoded['Messaggio']
+                            ];
+        }
+    }
+
+    public function clear_log(){
+        // TODO far entrare la datetime nel modo giusto
+        $stmt = $this->db->prepare('DELETE FROM Log WHERE timestamp < ?');
     }
 
 }
