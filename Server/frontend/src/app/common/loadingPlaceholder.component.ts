e5bda3246d574e5c55deeac243454264bbf8e6db
bracco23 <emiddio.esposito@hotmail.it>
2017-10-26 10:49:39 +0200

    aggiunto component loading, disattivata chiusura tesseramento

---
+++ Server/frontend/src/app/common/loadingPlaceholder.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+    selector: 'loading-placeholder',
+    template: `
+    <div *ngIf="!loaded" class="centered">
+        <mat-spinner></mat-spinner>
+        <h3 style="padding: 10px 0">Loading...</h3>
+    </div>
+    `,
+    styleUrls: ['../common/style.css'],
+    styles: [
+        `.mat-spinner{
+            margin: 0 auto;
+        }`
+    ]
+})
+export class LoadingPlaceholderComponent {
+}
