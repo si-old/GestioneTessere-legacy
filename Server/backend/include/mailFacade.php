6b5b0e7a80b7048be55d42a22f623898d348ef8f
bracco23 <emiddio.esposito@hotmail.it>
2018-03-19 23:11:05 +0100

    refactoring mail.php per separare responsabilità

---
+++ Server/backend/include/mailFacade.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once 'PHPMailer/src/PHPMailer.php';
+require_once 'PHPMailer/src/Exception.php';
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+class MailFacade
+{
+
+    private $EMAIL_OPTIONS = array(
+        'TITLE' => 'StudentIngegneria',
+        'URL' => 'http://www.Studentingegneria.it',
+        'FROM' => 'info@studentingegneria.it',
+        'CHARSET_UTF8' => 'utf-8',
+        'TYPE' => 'text/html',
+        'MULTIPART_TYPE' => 'multipart/mixed',
+    );
+
+    public function __construct()
+    {
+        $this->mailer = new PHPMailer(true);
+        $mail->setFrom($this->EMAIL_OPTIONS['FROM'], $this->EMAIL_OPTIONS['TITLE']);
+    }
+
+    public function set_body($body)
+    {
+        $email_body = str_replace("\\", "", nl2br($body));
+        $this->mailer->Body = $email_body;
+    }
+
+    public function add_attachments($attachments)
+    {
+        if (!is_iterable($attachments)) {
+            throw new InvalidArgumentException();
+        }
+        foreach ($attachments as $file) {
+            $this->mailer->addAttachment($file, basename($file));
+        }
+    }
+
+    public function set_subject($subject)
+    {
+        $subject_tmp = str_replace("\\", "", $subject);
+        $this->mailer->subject = $subject_tmp;
+    }
+
+    public function send_one($to, $display_name = null)
+    {
+        if (is_null($display_name)) {
+            $display_name = $to;
+        }
+        $this->mailer->addAddress($to, $display_name);
+        $result = $this->mailer->send();
+        $this->mailer->clearAddresses();
+        return $result;
+    }
+
+    public function send_list($to_list)
+    {
+        $count_ok = 0;
+        $count_nok = 0;
+        foreach ($to_list as $user) {
+            try {
+                $this->mailer->addAddress($user);
+                if ($this->mailer->send()) {
+                    $count_ok = $count_ok + 1;
+                } else {
+                    $count_nok = $count_nok + 1;
+                }
+                $this->mailer->clearAddresses();
+            } catch (Exception $e) {
+                $count_nok = $count_nok + 1;
+            }
+
+        }
+        return array('ok' => $count_ok, 'nok' => $count_nok);
+    }
+}
--- Server/backend/mail.php
+++ Server/backend/mail.php
@@ -2,10 +2,7 @@
 require_once 'include/config.php';
 require_once 'include/lib.php';
 require_once 'include/utils.php';
-require_once ('include/PHPMailer/src/PHPMailer.php');
-require_once ('include/PHPMailer/src/Exception.php');
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+require_once 'include/mailFacade.php';
 
 class Mail extends RESTItem
 {
@@ -18,23 +15,6 @@ class Mail extends RESTItem
     private $query_tessere = '	SELECT	t.Socio as t_socio, t.ID as t_id, t.Numero as t_numero,
 											a.ID as a_id, a.Anno as a_anno, a.Aperto as a_aperto
 									FROM Tessera as t JOIN Tesseramento as a on t.Anno = a.ID ';
-
-    private $EMAIL_OPTIONS = array(
-        'TITLE' => 'StudentIngegneria',
-        'URL' => 'http://www.Studentingegneria.it',
-        'FROM' => 'info@studentingegneria.it',
-        'CHARSET_UTF8' => 'utf-8',
-        'TYPE' => 'text/html',
-        'MULTIPART_TYPE' => 'multipart/mixed',
-    );
-
-    private $SUPPORTED_FILE_TYPES = array(
-        ".doc" => "application/msword",
-        ".jpg" => "image/jpeg",
-        ".gif" => "image/gif",
-        ".zip" => "application/zip",
-        ".pdf" => "application/pdf",
-    );
 
     private $TMP_ATTACH_DIR = './tmp_attachments';
 
@@ -59,36 +39,39 @@ class Mail extends RESTItem
         $valid = $valid && isset($data['blacklist']) && isset($data['tutti']) && isset($data['lavoratori']);
         $valid = ($valid && $data['tutti']) || ($valid && !$data['tutti'] && isset($data['corsi']));
         if ($valid) {
-            $mail = new PHPMailer(true); 
-            $mail->setFrom($this->EMAIL_OPTIONS['FROM'], $this->EMAIL_OPTIONS['TITLE']);
+            $mail = new MailFacade();
+
+            $mail->set_body($data["corpo"]);
             if (isset($data['files'])) {
-                $this->addAttachment($mail, $data['files']);		
+                $mail->add_attachment($data['files']);
             }
-            $subject_tmp = $data['oggetto'];
-            $subject = str_replace("\\", "", $subject_tmp);
-            $admin_mail = $data['email_feedback'];
-            $mail->addAddress($admin_mail, 'Admin mail');
-            $admin_subject = "[admin] " . $subject;
-            $mail->Subject = $admin_subject;
-            $email_body_tmp = nl2br($data['corpo']);
-            $email_body = str_replace("\\", "", $email_body_tmp);
-            $mail->Body = $email_body;
+            
+            $mail->set_subject("[admin] " . $data['oggetto']);
             try {
-                $mail->send();
-                $mail->clearAddresses();
+                $mail->send_one($data['email_feedback'], "Admin email");
+                $this->log_info("Invio e-mail all'indirizzo di feedback.");
             } catch (Exception $e) {
                 throw new RESTException(HttpStatusCode::$INTERNAL_SERVER_ERROR, $mail->ErrorInfo);
             }
-            $this->log_info("Invio e-mail all'indirizzo di feedback.");
+
             if (isset($data['corsi'])) {
                 $corsi = $data['corsi'];
             } else {
                 $corsi = '';
             }
-            $this->log_info("Invio e-mail con i parametri: oggetto->" . $data['oggetto'] . ", blacklist->" . $data['blacklist'] . ", tutti->" . $data['tutti'] . ", lavoratori->" . $data['lavoratori'] . ".");
+            $this->log_info("Invio e-mail con i parametri: " .
+                            "oggetto->" . $data['oggetto'] . 
+                            ", blacklist->" . $data['blacklist'] . 
+                            ", tutti->" . $data['tutti'] . 
+                            ", lavoratori->" . $data['lavoratori']
+                        );
             $users = $this->get_users($data['blacklist'], $data['tutti'], $corsi, $data['lavoratori']);
-            $return_value = $this->send_mails($users, $subject, $mail);
+            
+            $mail->set_subject($data['oggetto']);
+            $return_value = $mail->send_list($users);
+
             $this->clear_tmp_dir();
+            
             return $return_value;
         } else {
             throw new RESTException(HttpStatusCode::$BAD_REQUEST, "Request JSON object is missing or has a wrong format");
@@ -120,8 +103,7 @@ class Mail extends RESTItem
                 foreach ($files as $index => $file) {
                     $new_path = $this->TMP_ATTACH_DIR . '/' . $_FILES[$file]['name'];
                     move_uploaded_file($_FILES[$file]['tmp_name'], $new_path);
-                    $content['files'][$index]['name'] = $_FILES[$file]['name'];
-                    $content['files'][$index]['path'] = $new_path;
+                    $content['files'][$index] = $new_path;
                 }
             }
         } else {
@@ -132,20 +114,12 @@ class Mail extends RESTItem
 
     private function clear_tmp_dir()
     {
-        $files = glob($this->TMP_ATTACH_DIR.'/*'); // get all file names
+        $files = glob($this->TMP_ATTACH_DIR . '/*'); // get all file names
         foreach ($files as $file) { // iterate files
             if (is_file($file)) {
                 unlink($file);
-            }// delete file
+            } // delete file
         }
-    }
-
-    private function addAttachment($mail, $files)
-    {
-        foreach ($files as $file) {
-        	$filepath = $file['path'];
-	        $mail->addAttachment($filepath, basename($filepath));
-	    }
     }
 
     private function get_users($blacklist, $all, $cdl_whitelist, $workers)
@@ -177,40 +151,17 @@ class Mail extends RESTItem
             throw new RESTException(HttpStatusCode::$INTERNAL_SERVER_ERROR, $this->db->error);
         }
         $results = fetch_results($stmt);
-        return $results;
+        return array_map(function ($user) {
+                            return $user['s_mail'];
+                        }, $results);
     }
 
     private function get_list($corsi)
     {
-        $corsi_ids = '';
-        foreach ($corsi as $id) {
-            $corsi_ids = $corsi_ids . intval($id) . ', ';
-        }
-        $corsi_ids = substr($corsi_ids, 0, -2);
-        return $corsi_ids;
-    }
-
-    private function send_mails($users, $subject, $mail)
-    {
-        $count_ok = 0;
-        $count_nok = 0;
-        $mail->Subject = $subject;
-        foreach ($users as $user) {
-            try {
-                $mail->addAddress($user['s_email']);
-                if ($mail->send()) {
-                    $count_ok = $count_ok + 1;
-                } else {
-                    $count_nok = $count_nok + 1;
-                }
-                $mail->clearAddresses();
-            } catch (Exception $e) {
-                $count_nok = $count_nok + 1;
-            }
-
-        }
-        $this->log_info("Inviata e-mail a $count_ok soci.");
-        return array('ok' => $count_ok, 'nok' => $count_nok);
+        $corsi_int = array_map(function ($corso){
+            return intval($corso);
+        }, $corsi);
+        return implode(', ', $corsi_int);
     }
 }
 
