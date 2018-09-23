87f63384bd7d4a77e0c567650b662643924124ba
bracco23 <emiddio.esposito@hotmail.it>
2018-09-23 19:08:37 +0200

    aggiunto supporto per statini per tesseramento

--- Server/backend/statino.php
+++ Server/backend/statino.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once('include/include.php');
+require_once 'include/include.php';
 
 use \setasign\Fpdi\Fpdi;
 
 $query = "SELECT t.Numero as t_numero, te.Anno as te_anno, ca.Studente as ca_studente,
                 ca.Professione as ca_professione, ca.Matricola as ca_matricola,
-                c.Nome as c_nome, st.Nome as st_nome, st.Cognome as st_cognome, 
+                c.Nome as c_nome, st.Nome as st_nome, st.Cognome as st_cognome,
                 st.Email as st_email, st.Cellulare as st_cellulare,
                 st.Quota as st_quota, DATE_FORMAT(st.Data, '%d/%m/%Y') as st_data
             FROM Statino as st JOIN Tessera as t on st.Tessera = t.ID
@@ -14,11 +14,15 @@ $query = "SELECT t.Numero as t_numero, te.Anno as te_anno, ca.Studente as ca_stu
                                JOIN Carriera as ca on st.Carriera = ca.ID
                                JOIN Corso as c on ca.Corso = c.ID";
 
-if(isset($_GET['id']) && strlen($_GET['id']) > 0){
-    $query = $query.' WHERE st.ID = ?';
+if (isset($_GET['id']) && strlen($_GET['id']) > 0) {
+    $query = $query . ' WHERE st.ID = ?';
     $stmt = $db->prepare($query);
     $stmt->bind_param('i', $_GET['id']);
-}else{
+} else if (isset($_GET['anno']) && strlen($_GET['anno']) > 0) {
+    $query = $query . ' WHERE te.ID = ?';
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('i', $_GET['anno']);
+} else {
     $stmt = $db->prepare($query);
 }
 
@@ -26,56 +30,62 @@ $stmt->execute();
 
 $res = fetch_results($stmt);
 
-$pdf = new Fpdi();
+if (count($res) > 0) {
 
-foreach ($res as $statino){
-    $pdf->AddPage();
-    $pdf->setSourceFile("res/template_statino.pdf");
-    $tplIdx = $pdf->importPage(1);
-    $pdf->useTemplate($tplIdx, ['adjustPageSize' => true]);
-    $pdf->SetFont('Helvetica');
-    
-    $pdf->SetXY(31, 26); // tutte i numeri sono mm a partire dall'angolo in alto a sinistra
-    $pdf->Write(0, $statino['t_numero']);
+    $pdf = new Fpdi();
 
-    $pdf->SetXY(45, 33);
-    $pdf->Write(0, $statino['te_anno']);
+    foreach ($res as $statino) {
+        $pdf->AddPage();
+        $pdf->setSourceFile("res/template_statino.pdf");
+        $tplIdx = $pdf->importPage(1);
+        $pdf->useTemplate($tplIdx, ['adjustPageSize' => true]);
+        $pdf->SetFont('Helvetica');
 
-    if($statino['ca_studente']){
-        $pdf->SetXY(13, 41);
-        $pdf->Write(0, 'x');
+        $pdf->SetXY(31, 26); // tutte i numeri sono mm a partire dall'angolo in alto a sinistra
+        $pdf->Write(0, $statino['t_numero']);
 
-        $pdf->SetXY(45, 40);
-        $pdf->Write(0, $statino['c_nome']);
+        $pdf->SetXY(45, 33);
+        $pdf->Write(0, $statino['te_anno']);
 
-        $pdf->SetXY(30, 65);
-        $pdf->Write(0, $statino['ca_matricola']);
-    }else{
-        $pdf->SetXY(13, 47);
-        $pdf->Write(0, 'x');
+        if ($statino['ca_studente']) {
+            $pdf->SetXY(13, 41);
+            $pdf->Write(0, 'x');
 
-        $pdf->SetXY(55, 46);
-        $pdf->Write(0, $statino['ca_professione']);
+            $pdf->SetXY(45, 40);
+            $pdf->Write(0, $statino['c_nome']);
+
+            $pdf->SetXY(30, 65);
+            $pdf->Write(0, $statino['ca_matricola']);
+        } else {
+            $pdf->SetXY(13, 47);
+            $pdf->Write(0, 'x');
+
+            $pdf->SetXY(55, 46);
+            $pdf->Write(0, $statino['ca_professione']);
+        }
+
+        $pdf->SetXY(34, 57);
+        $pdf->Write(0, $statino['st_nome']);
+
+        $pdf->SetXY(82, 57);
+        $pdf->Write(0, $statino['st_cognome']);
+
+        $pdf->SetXY(25, 79);
+        $pdf->Write(0, $statino['st_email']);
+
+        $pdf->SetXY(28, 72);
+        $pdf->Write(0, $statino['st_cellulare']);
+
+        $pdf->SetXY(85, 118);
+        $pdf->Write(0, $statino['st_quota']);
+
+        $pdf->SetXY(26, 140);
+        $pdf->Write(0, $statino['st_data']);
+
     }
-    
-    $pdf->SetXY(34, 57);
-    $pdf->Write(0, $statino['st_nome']);
 
-    $pdf->SetXY(82, 57);
-    $pdf->Write(0, $statino['st_cognome']);
-
-    $pdf->SetXY(25, 79);
-    $pdf->Write(0, $statino['st_email']);
-
-    $pdf->SetXY(28, 72);
-    $pdf->Write(0, $statino['st_cellulare']);
-
-    $pdf->SetXY(85, 118);
-    $pdf->Write(0, $statino['st_quota']);
-
-    $pdf->SetXY(26, 140);
-    $pdf->Write(0, $statino['st_data']);
-    
+    $pdf->Output();
+}else{
+    header( "refresh:5;url=".$_SERVER['HTTP_REFERER']);
+    echo '<h1>Nessuno statino trovato!</h1>';
 }
-
-$pdf->Output();            
