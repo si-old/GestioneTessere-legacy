08b649e15a5ca527d051e046d45dc32be157d57b
bracco23 <emiddio.esposito@hotmail.it>
2018-10-13 19:15:14 +0200

    aggiunta visualizzazione link per url e mail

--- Server/frontend/src/app/viewedit/text.component.ts
+++ Server/frontend/src/app/viewedit/text.component.ts
@@ -11,6 +11,19 @@ import {
 
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
+function ValidURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name and extension
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?'+ // port
+  '(\\/[-a-z\\d%@_.~+&:]*)*'+ // path
+  '(\\?[;&a-z\\d%@_.,~+&:=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return pattern.test(str);
+}
+
+import * as EmailValidator from 'email-validator';
+
 const noop = () => {
 };
 
@@ -54,6 +67,9 @@ export class TextVieweditComponent implements ControlValueAccessor{
 
     @Input() flag: boolean;
 
+    url: boolean = false;
+    email: boolean = false;
+
     private onTouchedCallback: () => void = noop;
     private onChangeCallback: (_: any) => void = noop;
 
@@ -63,6 +79,8 @@ export class TextVieweditComponent implements ControlValueAccessor{
         if (this._content !== in_content) {
             this._content = in_content;
             this.onChangeCallback(this._content);
+            this.url = ValidURL(in_content)
+            this.email = EmailValidator.validate(in_content)
         }
     }
 
